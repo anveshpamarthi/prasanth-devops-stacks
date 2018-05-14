b0f142c719df38c3c71e4dddec8428284217fa87
prasanthch8 <31741023+prasanthch8@users.noreply.github.com>
2018-05-14 10:45:28 -0500

    adding file jenkinsfile4

---
+++ jenkisfile4.groovy
@@ -0,0 +1,33 @@
+import org.apache.commons.lang.RandomStringUtils
+pipeline
+{
+    agent any
+    stages{
+        stage('AWS-S3-Upload'){
+            steps{
+                withAWS(credentials: "altuspoc", region: "us-east-2"){
+                    s3Upload bucket: 'cfscripts-altus-as', file: 'awssqs.yaml', path: 'AS06/sqs.yaml'
+                }
+            }
+        }
+		stage('Validate-Run-CF-Templates'){
+			steps{
+				withAWS(credentials: "altuspoc", region: "us-east-2"){
+					cfnValidate(file: 'Master2.yaml')
+					cfnUpdate(stack: "PipeSqsDemo", create: true, file: 'Master2.yaml', timeoutInMinutes: 5)
+				}
+			}
+		}
+    }
+    post{
+        always{
+            echo "Deployment is run for $env.Environment"
+        }
+        failure{
+            echo "Error occured"
+        }
+        success{
+            echo "Succesfully deployed to $env.Environment"
+        }
+    }
+}
